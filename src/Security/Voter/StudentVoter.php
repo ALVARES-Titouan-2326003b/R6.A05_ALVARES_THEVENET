56870bc7dd9cf4eaeabd8937ea46ff42ff200e1f
ALVARES-Titouan-2326003b <titouan.alvares@etu.univ-amu.fr>
2026-02-10 12:14:18 +0100

    accès aux pages selon le rôle

---
+++ src/Security/Voter/StudentVoter.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class StudentVoter extends Voter
+{
+    const IS_STUDENT = 'IS_STUDENT';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        return $attribute === self::IS_STUDENT;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        return !$user->getTeacher();
+    }
+}
